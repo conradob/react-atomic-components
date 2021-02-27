0b6a7048083235820cf986cc03ff42d3b245d8dd
Conrado Fonseca <conradob@live.com>
2021-02-27 14:21:03 +0000

    Integrate react-router-dom in App and created new route for issues screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import {Header} from 'components/Header';
 import {Layout} from 'components/Layout';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Issues} from 'screens/issues';
 
 function App() {
   return (
     <Layout>
-      <Header>
-        <p>Omnipresent Frontend Challenge</p>
-      </Header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Header>
+              <p>Omnipresent Frontend Challenge</p>
+            </Header>
+          </Route>
+          <Route path="/:owner/:repo/issues" children={<Issues />} />
+        </Switch>
+      </Router>
     </Layout>
   );
 }
